b32c9b41b1ac3db84c378ac344a5e94717c21aa5
midbel <nbrun@gld.be>
2025-07-04 21:41:12 +0200

    missing semicolon

--- internal/lang/parser/testdata/cte.sql
+++ internal/lang/parser/testdata/cte.sql
@@ -2,7 +2,21 @@ with persons1 as (select firstname, lastname from employees) select firstname fr
 
 with persons2 as (
 	select id, firstname, lastname from employees
-), emails as (
+), emails1 as (
 	select id, mail from emails
 )
-select t1.firstname, t2.mail from persons2 t1 join emails t2 on t1.id=t2.id;
+select t1.firstname, t2.mail from persons2 t1 join emails1 t2 on t1.id=t2.id;
+
+with persons3 as (
+	select id, firstname, lastname from employees
+), emails2 as (
+	select id, mail from emails
+), contacts as (
+	select 
+		t1.firstname, 
+		t2.mail 
+	from persons3 t1 
+	join emails2 t2 on t1.id=t2.id
+	join domains d on t2.scheme=d.scheme
+)
+select * from contacts;
