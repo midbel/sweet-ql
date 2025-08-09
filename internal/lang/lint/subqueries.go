540d018dca41bb59f6c3725b78abd19166451f37
midbel <nbrun@gld.be>
2025-08-09 22:15:18 +0200

    check exported names

--- internal/lang/lint/subqueries.go
+++ internal/lang/lint/subqueries.go
@@ -5,6 +5,7 @@ import (
 	"slices"
 
 	"github.com/midbel/sweet/internal/lang/ast"
+	"github.com/midbel/sweet/internal/slx"
 )
 
 type subqueryColumnsCount struct {
@@ -113,13 +114,122 @@ func (r *subqueryNames) Verify(stmt ast.Node) ([]Issue, error) {
 	return r.issues, err
 }
 
-func (r *subqueryNames) VisitJoin(join ast.Join) error {
+func (r *subqueryNames) VisitSelect(stmt ast.SelectStatement) error {
+	for _, t := range stmt.Tables {
+		if err := r.visitNode(t, stmt); err != nil {
+			return err
+		}
+	}
+	return errVisit
+}
+
+func (r *subqueryNames) visitNode(node ast.Node, stmt ast.SelectStatement) error {
+	x := &subqueryJoinNames{
+		Visitor: ast.Noop(),
+	}
+	if err := node.Accept(Walk(x)); err != nil {
+		return err
+	}
+	if len(x.names) == 0 {
+		return nil
+	}
+	q := &queryIdentUsage{
+		Visitor: ast.Noop(),
+		names:   x.names,
+		alias:   x.alias,
+	}
+	if err := stmt.Accept(Walk(q)); err != nil {
+		return err
+	}
+	for i := range q.issues {
+		q.issues[i].Severity = r.severity
+		q.issues[i].Rule = r.Name()
+	}
+	r.issues = slices.Concat(r.issues, q.issues)
 	return nil
 }
 
-func (r *subqueryNames) VisitGroup(group ast.Group) error {
-	if _, ok := group.Node.(ast.SelectStatement); ok {
+type queryIdentUsage struct {
+	ast.Visitor
+	issues []Issue
+	names  []ast.Name
+	alias  ast.Identifier
+}
 
+func (s *queryIdentUsage) VisitSelect(stmt ast.SelectStatement) error {
+	for _, c := range stmt.Columns {
+		if err := c.Accept(s); err != nil {
+			return err
+		}
+	}
+	if stmt.Where != nil {
+		if err := stmt.Where.Accept(s); err != nil {
+			return err
+		}
+	}
+	for _, g := range stmt.Groups {
+		if err := g.Accept(s); err != nil {
+			return err
+		}
+	}
+	if stmt.Having != nil {
+		if err := stmt.Having.Accept(s); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (s *queryIdentUsage) VisitName(name ast.Name) error {
+	if len(name.Parts) <= 1 {
+		return nil
+	}
+	if name.Parts[len(name.Parts)-2] != s.alias {
+		return nil
+	}
+	ok := slices.ContainsFunc(s.names, func(n ast.Name) bool {
+		return n.Parts[len(n.Parts)-1] == name.Parts[len(name.Parts)-1]
+	})
+	if !ok {
+		i := Issue{
+			Position: name.Pos(),
+			Reason:   "identifier is not defined in subquery",
+		}
+		s.issues = append(s.issues, i)
+	}
+	return nil
+}
+
+type subqueryJoinNames struct {
+	ast.Visitor
+
+	names []ast.Name
+	alias ast.Identifier
+}
+
+func (s *subqueryJoinNames) VisitAlias(alias ast.Alias) error {
+	s.alias = alias.Identifier
+
+	entry := alias.Node
+	if g, ok := entry.(ast.Group); ok {
+		entry = g.Node
+	} else {
+		return nil
+	}
+	if stmt, ok := entry.(ast.SelectStatement); ok {
+		for _, c := range stmt.Columns {
+			switch c := c.(type) {
+			case ast.Name:
+				s.names = append(s.names, c)
+			case ast.Alias:
+				n := ast.Name{
+					Position: c.Pos(),
+					Parts:    slx.One(c.Identifier),
+				}
+				s.names = append(s.names, n)
+			default:
+			}
+		}
 	}
 	return nil
 }
