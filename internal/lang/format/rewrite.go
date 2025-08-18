2c93953a85c20eb5f643ab84f20896cb8bfa2397
midbel <nbrun@gld.be>
2025-08-18 21:43:32 +0200

    rewriter factory

--- internal/lang/format/rewrite.go
+++ internal/lang/format/rewrite.go
@@ -1,11 +1,42 @@
 package format
 
 import (
+	"fmt"
+
 	"github.com/midbel/sweet/internal/lang/ast"
 )
 
 type Rewriter interface {
 	Rewrite(ast.Node) (ast.Node, error)
+}
+
+var factory = map[string]func() Rewriter{
+	"std-operator":         StdOperator,
+	"cte-to-subquery":      nil,
+	"subquery-to-cte":      nil,
+	"groupby-field":        nil,
+	"groupby-aggr":         nil,
+	"groupby-pos":          nil,
+	"missing-alias-fields": MissingAliasFields,
+	"missing-alias-tables": MissingAliasTables,
+	"missing-cte-fields":   MissingCteFields,
+	"missing-view-fields":  MissingViewFields,
+	"placeholder-join":     ReplaceLiteralJoin,
+	"placeholder-where":    ReplaceLiteralWhere,
+	"no-returning":         NoReturning,
+	"limit-to-fetch":       nil,
+	"add-primary-key":      nil,
+}
+
+func RewriterByName(name string) (Rewriter, error) {
+	fn, ok := factory[name]
+	if ok {
+		if fn == nil {
+			return nil, fmt.Errorf("%s: not yet implemented", name)
+		}
+		return fn(), nil
+	}
+	return nil, fmt.Errorf("%s: unsupported rewriter", name)
 }
 
 // rewrite operator to std one like != to <> and = true to is true
@@ -58,65 +89,225 @@ func (r rewriteStdOperator) TransformBinary(binary *ast.Binary) (ast.Node, error
 
 // transform all subqueries to an equivalent cte
 type rewriteSubqueryToCte struct {
-	ast.Visitor
+	ast.Transformer
+}
+
+func (r rewriteSubqueryToCte) Rewrite(stmt ast.Node) (ast.Node, error) {
+	t, ok := stmt.(ast.TransformableNode)
+	if !ok {
+		return stmt, nil
+	}
+	walker := ast.Transform(r)
+	return t.Transform(walker)
 }
 
 // transform all cte to subquery where they are used
 type rewriteCteToSubquery struct {
-	ast.Visitor
+	ast.Transformer
+}
+
+func (r rewriteCteToSubquery) Rewrite(stmt ast.Node) (ast.Node, error) {
+	t, ok := stmt.(ast.TransformableNode)
+	if !ok {
+		return stmt, nil
+	}
+	walker := ast.Transform(r)
+	return t.Transform(walker)
 }
 
 // add fields not use in aggregate function in the select clause into the group by
 type rewriteGroupbyFields struct {
-	ast.Visitor
+	ast.Transformer
 }
 
-func (r rewriteGroupbyFields) VisitSelect(stmt *ast.SelectStatement) error {
-	return nil
+func (r rewriteGroupbyFields) Rewrite(stmt ast.Node) (ast.Node, error) {
+	t, ok := stmt.(ast.TransformableNode)
+	if !ok {
+		return stmt, nil
+	}
+	walker := ast.Transform(r)
+	return t.Transform(walker)
+}
+
+func (r rewriteGroupbyFields) TransformSelect(stmt *ast.SelectStatement) (ast.Node, error) {
+	return stmt, nil
 }
 
 // when position are used in group by, replace by the identifier
 type rewriteGroupbyPosField struct {
-	ast.Visitor
+	ast.Transformer
 }
 
-func (r rewriteGroupbyPosField) VisitSelect(stmt *ast.SelectStatement) error {
-	return nil
+func (r rewriteGroupbyPosField) Rewrite(stmt ast.Node) (ast.Node, error) {
+	t, ok := stmt.(ast.TransformableNode)
+	if !ok {
+		return stmt, nil
+	}
+	walker := ast.Transform(r)
+	return t.Transform(walker)
 }
+
+func (r rewriteGroupbyPosField) TransformSelect(stmt *ast.SelectStatement) (ast.Node, error) {
+	return stmt, nil
+}
+
+type AliasMode int8
+
+const (
+	AliasFields AliasMode = 1 << iota
+	AliasTables
+	AliasBoth
+)
 
 // add alias to all columns in select clause
 type rewriteMissingAlias struct {
-	ast.Visitor
+	ast.Transformer
+	mode AliasMode
 }
+
+func MissingAliasFields() Rewriter {
+	return rewriteMissingAlias{
+		Transformer: ast.Keep(),
+		mode:        AliasFields,
+	}
+}
+
+func MissingAliasTables() Rewriter {
+	return rewriteMissingAlias{
+		Transformer: ast.Keep(),
+		mode:        AliasTables,
+	}
+}
+
+func (r rewriteMissingAlias) Rewrite(stmt ast.Node) (ast.Node, error) {
+	t, ok := stmt.(ast.TransformableNode)
+	if !ok {
+		return stmt, nil
+	}
+	walker := ast.Transform(r)
+	return t.Transform(walker)
+}
+
+func (r rewriteMissingAlias) TransformSelect(stmt *ast.SelectStatement) (ast.Node, error) {
+	return stmt, nil
+}
+
+type MissingMode int8
+
+const (
+	CteMissing MissingMode = 1 << iota
+	ViewMissing
+	AllMissing
+)
 
 // add columns definition list to cte
 type rewriteMissingColumnsNames struct {
-	ast.Visitor
+	ast.Transformer
+	mode MissingMode
 }
 
-func (r rewriteMissingColumnsNames) VisitCte(stmt *ast.CteStatement) error {
-	return nil
+func MissingCteFields() Rewriter {
+	return rewriteMissingColumnsNames{
+		Transformer: ast.Keep(),
+		mode:        CteMissing,
+	}
 }
 
-func (r rewriteMissingColumnsNames) VisitCreateView(stmt *ast.CreateViewStatement) error {
-	return nil
+func MissingViewFields() Rewriter {
+	return rewriteMissingColumnsNames{
+		Transformer: ast.Keep(),
+		mode:        ViewMissing,
+	}
+}
+
+func (r rewriteMissingColumnsNames) Rewrite(stmt ast.Node) (ast.Node, error) {
+	t, ok := stmt.(ast.TransformableNode)
+	if !ok {
+		return stmt, nil
+	}
+	walker := ast.Transform(r)
+	return t.Transform(walker)
+}
+
+func (r rewriteMissingColumnsNames) TransformCte(stmt *ast.CteStatement) (ast.Node, error) {
+	return stmt, nil
+}
+
+func (r rewriteMissingColumnsNames) TransformCreateView(stmt *ast.CreateViewStatement) (ast.Node, error) {
+	return stmt, nil
 }
 
 // rewrite literal value in join with placeholders
-type rewriteLiteralWithPlaceholderJoin struct {
-	ast.Visitor
+type rewriteLiteralWithPlaceholder struct {
+	ast.Transformer
 }
 
-// rewrite literal value in expression with placeholders
-type rewriteLiteralWithPlaceholderExpr struct {
-	ast.Visitor
+func ReplaceLiteralJoin() Rewriter {
+	return rewriteLiteralWithPlaceholder{
+		Transformer: ast.Keep(),
+	}
+}
+
+func ReplaceLiteralWhere() Rewriter {
+	return rewriteLiteralWithPlaceholder{
+		Transformer: ast.Keep(),
+	}
+}
+
+func (r rewriteLiteralWithPlaceholder) Rewrite(stmt ast.Node) (ast.Node, error) {
+	t, ok := stmt.(ast.TransformableNode)
+	if !ok {
+		return stmt, nil
+	}
+	walker := ast.Transform(r)
+	return t.Transform(walker)
 }
 
 // rewrite use of limit/offset to offset/fetch
 type rewriteLimitToFetch struct {
-	ast.Visitor
+	ast.Transformer
 }
 
-func (r rewriteLimitToFetch) VisitSelect(stmt *ast.SelectStatement) error {
-	return nil
+func (r rewriteLimitToFetch) Rewrite(stmt ast.Node) (ast.Node, error) {
+	t, ok := stmt.(ast.TransformableNode)
+	if !ok {
+		return stmt, nil
+	}
+	walker := ast.Transform(r)
+	return t.Transform(walker)
+}
+
+func (r rewriteLimitToFetch) TransformSelect(stmt *ast.SelectStatement) (ast.Node, error) {
+	return stmt, nil
+}
+
+type rewriteReturning struct {
+	ast.Transformer
+}
+
+func NoReturning() Rewriter {
+	return rewriteReturning{
+		Transformer: ast.Keep(),
+	}
+}
+
+func (r rewriteReturning) Rewrite(stmt ast.Node) (ast.Node, error) {
+	t, ok := stmt.(ast.TransformableNode)
+	if !ok {
+		return stmt, nil
+	}
+	walker := ast.Transform(r)
+	return t.Transform(walker)
+}
+
+func (r rewriteReturning) TransformInsert(stmt *ast.InsertStatement) (ast.Node, error) {
+	return stmt, nil
+}
+
+func (r rewriteReturning) TransformUpdate(stmt *ast.UpdateStatement) (ast.Node, error) {
+	return stmt, nil
+}
+
+func (r rewriteReturning) TransformDelete(stmt *ast.DeleteStatement) (ast.Node, error) {
+	return stmt, nil
 }
