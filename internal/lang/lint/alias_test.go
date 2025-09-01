c60269cde7b96b7119e55f7842c85d0089322f4f
midbel <nbrun@gld.be>
2025-09-01 20:03:05 +0200

    testing alias rules

--- internal/lang/lint/alias_test.go
+++ internal/lang/lint/alias_test.go
@@ -63,17 +63,81 @@ func TestRecommandedAlias(t *testing.T) {
 }
 
 func TestMissingAlias(t *testing.T) {
-	t.Skip()
+	tests := []TestCase{
+		{
+			Query:  "select f.id as ident from foobar f",
+			Issues: 0,
+		},
+		{
+			Query:  "select id as ident from foobar",
+			Issues: 1,
+		},
+		{
+			Query:  "select id, name from foobar",
+			Issues: 3,
+		},
+		{
+			Query:  "with foo as (select id from foo) select * from foo",
+			Issues: 4,
+		},
+		{
+			Query:  "select id from foo where active=(select active from bar)",
+			Issues: 4,
+		},
+	}
+	runTests(t, tests, MissingAlias(Warning))
 }
 
 func TestNoAlias(t *testing.T) {
-	t.Skip()
+	tests := []TestCase{
+		{
+			Query:  "select id, name from foobar",
+			Issues: 0,
+		},
+		{
+			Query:  "with foo as (select id from foo) select * from foo",
+			Issues: 0,
+		},
+		{
+			Query:  "select name as foo from foobar",
+			Issues: 1,
+		},
+		{
+			Query:  "select f.name as foo from foobar f",
+			Issues: 2,
+		},
+		{
+			Query:  "with foo as (select id as i from foo) select * from foo",
+			Issues: 1,
+		},
+	}
+	runTests(t, tests, NoAlias(Warning))
 }
 
 func TestInvalidAlias(t *testing.T) {
-	t.Skip()
+	tests := []TestCase{
+		{
+			Query:  "select foo f from foobar",
+			Issues: 0,
+		},
+		{
+			Query:  "select foo f from foobar where f.active is true",
+			Issues: 1,
+		},
+	}
+	runTests(t, tests, InvalidAlias(Warning))
 }
 
 func TestUndefinedAlias(t *testing.T) {
-	t.Skip()
+	tests := []TestCase{
+		{
+			Query:  "select f.id from foobar f",
+			Issues: 0,
+		},
+		{
+			Query:  "select b.id from foobar f",
+			Issues: 1,
+		},
+	}
+	runTests(t, tests, UndefinedAlias(Warning))
 }
