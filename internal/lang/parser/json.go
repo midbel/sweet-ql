bccd8601436eefb8ca6e67b7bfd6c1fb235a09df
midbel <nbrun@gld.be>
2025-09-27 21:22:46 +0200

    skeleton to parse json related function

--- internal/lang/parser/json.go
+++ internal/lang/parser/json.go
@@ -1,1 +1,31 @@
 package parser
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/midbel/sweet/internal/lang/ast"
+)
+
+func (p *Parser) ParseJSON(name ast.Node) (ast.Node, error) {
+	n, ok := name.(*ast.Name)
+	if !ok {
+		return nil, p.Unexpected("xml", defaultReason)
+	}
+	var (
+		stmt ast.Node
+		err  error
+	)
+	switch strings.ToUpper(n.Name()) {
+	case "JSON_OBJECT":
+	case "JSON_OBJECTAGG":
+	case "JSON_ARRAY":
+	case "JSON_ARRAYAGG":
+	case "JSON_VALUE":
+	case "JSON_QUERY":
+	case "JSON_EXISTS":
+	default:
+		return nil, p.Unexpected("json", fmt.Sprintf("%s: undefined/unsupported xml functions"))
+	}
+	return stmt, err
+}
